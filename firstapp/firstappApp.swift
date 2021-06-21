04a8c90e679afc9b5ff69a3e6d302e34d9280d2a
fabian <fabiankmroh@gmail.com>
2021-06-21 09:08:23 +0900

    Initial Commit

---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by Fabian on 2021/06/21.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
